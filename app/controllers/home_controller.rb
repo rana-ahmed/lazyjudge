1ba54dbb6c1d54ee0b0fcc9691ae54377d18e938
Rana Ahmed <minuddinrana@gmail.com>
2015-09-23 15:14:28 +0600

    Add score_board

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,8 @@ class HomeController < ApplicationController
   def index
 
   end
+
+  def score_board
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get 'score_board' => 'home#score_board'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
