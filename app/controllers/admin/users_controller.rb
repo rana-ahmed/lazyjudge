51d9802458bb5ba62bb985465b4bd6ec03b88ffb
Rana Ahmed <minuddinrana@gmail.com>
2015-09-29 01:25:50 +0600

    Complete user#create action

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,26 @@
+class Admin::UsersController < ApplicationController
+  def index
+    @users = User.order(:role).all
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users_path
+    else
+      @users = User.order(:role).all
+      render :index
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to admin_users_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:user_name, :password_digest, :role)
+  end
+end
