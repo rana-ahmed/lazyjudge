840f769db3941dfae56fc6ab816bd9699ed04056
Rana Ahmed <minuddinrana@gmail.com>
2015-10-02 21:58:31 +0600

    Basic authenticaion complete

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-
+    #here we will call current_user_root
   end
 
   def score_board
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,32 @@
 class SessionsController < ApplicationController
   def create
-
+    user = User.find_by_user_name(user_parms[:user_name])
+    if user && user.authenticate(user_parms[:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Logged in!"
+      flash[:type] = "alert-success"
+      #here we will call current_user_root
+      redirect_to root_url
+    else
+      flash[:notice] = "User name or password invalid"
+      flash[:type] = "alert-warning"
+      redirect_to :back
+    end
   end
 
   def destroy
+    if current_user
+      session[:user_id] = nil
+      redirect_to root_url, notice: "Logged out!"
+    else
+      flash[:notice] = "Logged in first"
+      flash[:type] = "alert-warning"
+      redirect_to root_url
+    end
+  end
 
+  private
+  def user_parms
+    params.require(:user).permit(:user_name, :password)
   end
 end
