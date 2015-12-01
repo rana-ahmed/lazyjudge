bc78ebec49f07a3b1b939b77f7fc0c75a4596c91
Rana Ahmed <minuddinrana@gmail.com>
2015-12-01 11:17:18 +0600

    Make sure prevent infinity redirection if judge remove a team

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,10 @@ class ApplicationController < ActionController::Base
 
   private
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.where(id: session[:user_id]).take if session[:user_id]
+    #if admin remove the team
+    session[:user_type] = nil if @current_user.nil?
+    @current_user
   end
   helper_method :current_user
 
